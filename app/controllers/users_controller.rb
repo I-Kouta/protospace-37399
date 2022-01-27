75f98daab7f71fb3def1cdd4d60a564f8aa43736
I-Kouta <kouta.baseball0222@gmail.com>
2022-01-27 16:30:01 +0900

    01/27:protospace修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @prototypes = current_user.prototypes
+    #user = User.find(params[:id])
+    #@user = user
+    #@prototypes = user.prototypes
   end
 end
