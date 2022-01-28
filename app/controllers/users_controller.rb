a79fc9cf2e9bb23ae1503a7d85c6b6f5709c7f09
I-Kouta <kouta.baseball0222@gmail.com>
2022-01-28 16:45:40 +0900

    詳細ページへのアクセス修正/ログイン時のリダイレクトの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
   def show
-    binding.pry
-    @user = current_user
-    @prototypes = current_user.prototypes
-    #user = User.find(params[:id])
-    #@user = user
-    #@prototypes = user.prototypes
+    user = User.find(params[:id])
+    @user = user
+    @prototypes = user.prototypes
   end
 end
